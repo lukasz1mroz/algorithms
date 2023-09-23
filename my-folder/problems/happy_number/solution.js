8945a5fdcda140b356b4a0241bf86bfd18671f54
Łukasz <45930931+lukasz1mroz@users.noreply.github.com>
2023-09-23 05:52:21 +0000

    Sync LeetCode submission - Happy Number (javascript)

--- my-folder/problems/happy_number/solution.js
+++ my-folder/problems/happy_number/solution.js
@@ -8,11 +8,8 @@ var isHappy = function(n) {
     let count = 1
 
     while (result !== 1) {
-        // Split number for digits
-        digArr = result.toString().split('').map((dig) => parseInt(dig))
-        // Get sum of digit squares
+        digArr = result.toString().split('').map(Number)
         sum = digArr.reduce((acc, curr) => acc + curr**2, 0)
-        // Rurn false if ...?
         if (count > 1 && sum < 10 && sum !== 1) {
             return false
         }
