cd7296f218aad048fbc149edb5e4019f243a111b
Łukasz <45930931+lukasz1mroz@users.noreply.github.com>
2023-10-21 15:04:38 +0000

    Sync LeetCode submission - Count Complete Tree Nodes (typescript)

---
+++ my-folder/problems/count_complete_tree_nodes/solution.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function countNodes(root: TreeNode | null): number {
+    // What's the end case
+    if(!root) return 0
+
+    // What's the simpliest task
+    // How can I shrink the problem
+
+    return 1 + countNodes(root.left) + countNodes(root.right)
+};
