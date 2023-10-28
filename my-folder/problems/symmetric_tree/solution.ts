ee099852151f0b0c3d117ade0ada1be9210e0b85
Łukasz <45930931+lukasz1mroz@users.noreply.github.com>
2023-10-28 08:59:54 +0000

    Sync LeetCode submission - Symmetric Tree (typescript)

--- my-folder/problems/symmetric_tree/solution.ts
+++ my-folder/problems/symmetric_tree/solution.ts
@@ -13,11 +13,12 @@
  */
 
 function isSymmetric(root: TreeNode | null): boolean {
-    return isMirror(root, root)
-}
-
-const isMirror = (t1: TreeNode | null, t2: TreeNode | null): boolean => {
-    if(!t1 && !t2) return true
-    if(!t1 || !t2) return false
-    return (t1.val === t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
-}
+    const stack = [[root, root]]
+    while(stack.length) {
+        const [left, right] = stack.pop()
+        if (!left && !right) continue
+        if (!left || !right || left.val !== right.val) return false
+        stack.push([left.left, right.right], [left.right, right.left])
+    }
+    return true
+};
