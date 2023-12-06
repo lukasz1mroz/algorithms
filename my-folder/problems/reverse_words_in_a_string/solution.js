2f18d3c151aefd77e78832456386e09ba8e4ec25
Łukasz <45930931+lukasz1mroz@users.noreply.github.com>
2023-12-06 09:07:03 +0000

    Sync LeetCode submission - Reverse Words in a String (javascript)

--- my-folder/problems/reverse_words_in_a_string/solution.js
+++ my-folder/problems/reverse_words_in_a_string/solution.js
@@ -3,10 +3,20 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    return s
-    .split(' ')
-    .map(word => word.trim())
-    .filter(w => w.length > 0)
-    .reverse()
-    .join(' ')
+    const alphaReg = /\S+/
+    let outputStr = ''
+    let i = 0, j = 0
+
+    while(i < s.length) {
+        if(alphaReg.test(s[i])) {
+            j = i
+            while(alphaReg.test(s[j+1]) && j < s.length) {
+                j++
+            }
+            outputStr = `${s.slice(i, j+1)} ${outputStr}`
+            i = j
+        }
+        i++
+    }
+    return outputStr.trimEnd()
 };
