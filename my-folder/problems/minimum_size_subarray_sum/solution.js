af9617dd1d8628bd1af75ea1a37a37c37b75b3b1
Łukasz <45930931+lukasz1mroz@users.noreply.github.com>
2023-12-15 13:27:36 +0000

    Sync LeetCode submission - Minimum Size Subarray Sum (javascript)

---
+++ my-folder/problems/minimum_size_subarray_sum/solution.js
@@ -0,0 +1,19 @@
+var minSubArrayLen = function(target, nums) {
+    let left = 0
+    let right = 0
+    let sum = 0
+    let result = 0
+
+    while(right < nums.length) {
+        sum += nums[right]
+
+        while(sum >= target) {
+            let len = right - left + 1
+            if(result === 0 || len < result) result = len
+            sum -= nums[left]
+            left++ 
+        }
+        right++
+    }
+    return result
+};
