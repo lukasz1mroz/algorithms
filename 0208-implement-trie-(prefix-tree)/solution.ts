0f51474feffc78a533979fb51aa98f1f3aca9917
Łukasz <45930931+lukasz1mroz@users.noreply.github.com>
2024-03-11 06:35:24 +0000

    Sync LeetCode submission Runtime - 166 ms (81.27%), Memory - 70.5 MB (60.42%)

---
+++ 0208-implement-trie-(prefix-tree)/solution.ts
@@ -0,0 +1,58 @@
+class Trie {
+  root: TrieNode;
+
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word: string): void {
+    let node = this.root;
+    for (let char of word) {
+      if (!node.children[char]) {
+        node.children[char] = new TrieNode();
+      }
+      node = node.children[char];
+    }
+    node.isEndOfWord = true;
+  }
+
+  search(word: string): boolean {
+    let node = this.root;
+    for (let char of word) {
+      if (!node.children[char]) {
+        return false;
+      }
+      node = node.children[char];
+    }
+    return node.isEndOfWord;
+  }
+
+  startsWith(prefix: string): boolean {
+    let node = this.root;
+    for (let char of prefix) {
+      if (!node.children[char]) {
+        return false;
+      }
+      node = node.children[char];
+    }
+    return true;
+  }
+}
+
+class TrieNode {
+  children: { [key: string]: TrieNode };
+  isEndOfWord: boolean;
+
+  constructor() {
+    this.children = {};
+    this.isEndOfWord = false;
+  }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
